310486df828d3f6c549846d889864727247c97fb
toodam <ffrisk832@gmail.com>
2020-02-10 01:57:35 +0900

    publicとindex.jsの変更

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Champion extends React.Component
+{
+  render()
+  {
+    return (
+      <button>
+        <img src={this.props.name} alt={this.props.nameJP}></img>
+      </button>
+    )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  }
+}
+
+
+
+
+
+
+class Test extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = 
+    {
+      imageSource1:'amumu.png',
+      imageSource2:'lux.png', 
+      imageSource3:'nami.png', 
+      imageSource4:'taric.png', 
+      imageSource5:'singed.png', 
+    };
+  }
+  render()
+  {
+    const image6='black.png';
+
+
+
+    return (
+      <div>
+        <h1 className='title'>TFTシミュレーター</h1>
+        <Champion name={this.state.imageSource1} nameJP='アムム' />
+        <button onClick = {()=>this.setState({imageSource2: image6})}>
+          <img src={this.state.imageSource2} alt='ラックス'></img>
+        </button>
+        <button onClick = {()=>this.setState({imageSource3: image6})}>
+          <img src={this.state.imageSource3} alt='ナミ'></img>
+        </button>
+        <button onClick = {()=>this.setState({imageSource4: image6})}>
+          <img src={this.state.imageSource4} alt='タリック'></img>
+        </button>
+        <button onClick = {()=>this.setState({imageSource5: image6})}>
+          <img src={this.state.imageSource5} alt='シンジド'></img>
+        </button>
+      </div>
+    );
+
+  }
+}
+
+
+
+
+
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('root')
+);
